c0e97c3a553b6547d328b140774c447d82aa98ba
ramanji45 <24jr1a05am@gmail.com>
2026-04-17 22:56:47 +0530

    Update exp12.py

--- exp12.py
+++ exp12.py
@@ -1,18 +1,10 @@
-# Input string
+
 s = input("Enter a String: ")
-
-# Split the string into words
 words = s.split()
-
-# Store in dictionary (index : word)
 d = {}
 for i in range(len(words)):
     d[i] = words[i]
-
-# Join the words with '-'
 joined_string = "-".join(words)
-
-# Output
 print("Dictionary after splitting")
 print(d)
 
